eeba6fcd8884446aa9ca4baee0504dc32d443cce
SaltyFish-G <64099979+SaltyFish-G@users.noreply.github.com>
2020-04-21 14:30:27 -0500

    Add files via upload

---
+++ Dv_Arrive.m
@@ -0,0 +1,74 @@
+function [dvR,Beta,DELTA] = Dv_Arrive(vA,vP,h,dir)
+%
+% Calculate the delta v require to end up the parking orbit ( Planetary
+% Rendezous) 
+%
+% IMPORTANT
+% All input and output are not vector form
+%
+% INPUT
+% "vA"   Helicentric approach velocity (KM/s)
+% "vP"   The planet velocity (KM/s)  
+% "h"    The (minmmun) altitude of parking orbit (KM) 
+% "mu_p" The Gravtional constnat of the planet arrival to (KM^3/s^2)
+% "dir"  Direction of the planetary transfer
+%      'earth2mars' or 'e2m' Earth to Mars transfer (inner to outer)
+%      'mars2earth' or 'm2e' Mars to Earth transfer (outer ot inner)
+%
+% OUTPUT
+% "dvR"   Required delta v to arrivel 
+% "Beta"  The angle between apse line and asymptote line for hyperbola
+% "DELTA" Aiming ardius 'жд'
+%
+% NOTE : This function only returen the magnitude of deltia V relative to
+% the Planet. NOT HELICENTRIC !!! 
+%% Constants 
+
+muE = 398600.44;
+muM = 42828;
+radE  = 6378.14;
+radM  = 3389.90;
+mu = 0;
+Rp = 0;
+%% Input Check
+
+if(~ischar(dir))
+    error('Must indicate the planetary transfer direction "e2m" or "m2e"')
+end
+
+switch lower(dir)
+    case{'earth2mars','e2m'}
+        vinf = vP-vA;
+        mu = muM;
+        Rp = radM;
+    case{'mars2earth','m2e'}
+        vinf = vA - vP;
+        mu = muE;
+        Rp = radE;
+end
+
+if(mu ==0 || Rp == 0)
+    error('Something when wrong in function "Dv_Arrive" Check the input!')
+end
+
+%%
+rp = Rp + h;
+
+e = 1+rp*vinf^2/mu;
+
+h = rp*sqrt(vinf^2+2*mu/rp);
+
+DELTA = rp*sqrt(1+2*mu/(rp*vinf^2)); % aiming radius (eqn 8.57)
+
+vp_hyp = sqrt(vinf^2 + 2*mu/rp); % eqn 8.58
+
+vp_capture = sqrt(mu*(1+e)/rp); % eqn 8.59
+
+dvR = vp_hyp-vp_capture; % eqn 8.60
+
+Beta = acos(1/e);
+
+
+
+
+return
---
+++ Dv_Departure.m
@@ -0,0 +1,77 @@
+function [dvR,Beta,DELTA] =  Dv_Departure(vT,h,dir)    
+% 
+% Calculate the delta v require to departure from a planet (hyperbola
+% terojectory) 
+%
+% IMPORTANT
+% All input and output are not vector form
+%
+% INPUT
+% "vT,   The velocity aimed to reach (KM/s)
+% "h"    The altitude of parking orbit (KM) 
+% "dir"  Direction of the planetary transfer
+%      'earth2mars' or 'e2m' Earth to Mars transfer (inner to outer)
+%      'mars2earth' or 'm2e' Mars to Earth transfer (outer ot inner)
+%
+% OUTPUT
+% "dvR"   Required delta v to departure 
+% "Beta"  The angle between apse line and asymptote line for hyperbola
+% "DELTA" Aiming ardius 'жд'
+%
+% NOTE : This function only returen the magnitude of deltia V relative to
+% the Planet. NOT HELICENTRIC !!! 
+% 
+
+%% Input Check 
+if(~ischar(dir))
+    error('Must indicate the planetary transfer direction "e2m" or "m2e"')
+end
+
+%% Constants
+muE = 398600.44;
+muM = 42828;
+radE  = 6378.14;
+radM  = 3389.90;
+%% 
+    switch lower(dir)
+        case{'earth2mars','e2m'}
+
+        % departure fomr earth. circular parking orbit
+        mu_p = muE; 
+        R = radE+h;
+
+        Vc = sqrt(mu_p/R);              % parking orbit velocity(Assuming they are all circular)
+        vinf = vT;                      % hyperbolic excess velocity
+        h = R*sqrt(vinf^2+2*mu_p/R);    % angular momentum of hyperbola
+        e = 1+R*vinf^2/mu_p;            % eccentricity of hyperbola
+        vP = h/R;                       % velocity of hyperobola at perigee 
+        dvR = vP - Vc;                  % required delta-v from parking orbit
+        Beta = acos(1/e);               % angle between apse line and asymptote line for hyperbola
+        sma = h^2/mu_p/(1-e^2);         % Smei-major axis 
+        DELTA = (R+abs(sma))*sin(Beta); % aiming radius
+
+
+        case{'mars2earth','m2e'}
+
+         % Departure from Mars. From Planetary Rendezous. The capature orbit
+         % is not circular.
+         mu_p = muM;
+         R = radM + h;
+         vinf = vT;
+         h = R*sqrt(vinf^2+2*mu_p/R);    % angular momentum of hyperbola
+         e = 1+R*vinf^2/mu_p;            % eccentricity of hyperbola
+         vP = h/R;                       % velocity of hyperobola at perigee 
+         dvR = vinf - vP;                % required delta-v from capature orbit
+         Beta = acos(1/e);               % angle between apse line and asymptote line for hyperbola  
+         sma = h^2/mu_p/(1-e^2);         % Smei-major axis 
+         DELTA = (R+abs(sma))*sin(Beta); % aiming radius
+
+     % PS: THe capature orbit is a ellipical orbit. However, We don have
+     % enought information to calculate the orbit element for capature orbit.
+     % But we know that at perigee v_capiture and v_hyperbola is the same so
+     % delta_v require is simply vinf-vP 
+
+    end
+    
+    
+return 
