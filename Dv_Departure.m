50fea8508af7178f933baee21fcc48e53b64b4d2
SaltyFish-G <64099979+SaltyFish-G@users.noreply.github.com>
2020-04-28 18:39:44 -0500

    Add files via upload

--- Dv_Departure.m
+++ Dv_Departure.m
@@ -1,4 +1,4 @@
-function [dvR,Beta,DELTA] =  Dv_Departure(vT,h,dir)    
+function [dvR,Beta,DELTA] =  Dv_Departure(vT,vP,h,dir)    
 % 
 % Calculate the delta v require to departure from a planet (hyperbola
 % terojectory) 
@@ -7,7 +7,8 @@ function [dvR,Beta,DELTA] =  Dv_Departure(vT,h,dir)
 % All input and output are not vector form
 %
 % INPUT
-% "vT,   The velocity aimed to reach (KM/s)
+% "vT,   The velocity aimed to reach (KM/s)[3,1]
+% 'vP'   The velocity of the Planet (km/s) [3,1]
 % "h"    The altitude of parking orbit (KM) 
 % "dir"  Direction of the planetary transfer
 %      'earth2mars' or 'e2m' Earth to Mars transfer (inner to outer)
@@ -39,19 +40,21 @@ radM  = 3389.90;
         % departure fomr earth. circular parking orbit
         mu_p = muE; 
         R = radE+h;
+        vinf = norm(vT) - norm(vP);
         case{'mars2earth','m2e'}
         mu_p = muM;
         R = radM+h;
+        vinf = norm(vP) - norm(vT);
     end
     
     
     
         Vc = sqrt(mu_p/R);              % parking orbit velocity(Assuming they are all circular)
-        vinf = vT;                      % hyperbolic excess velocity
+        %vinf = norm(vT);                % hyperbolic excess velocity
         h = R*sqrt(vinf^2+2*mu_p/R);    % angular momentum of hyperbola
         e = 1+R*vinf^2/mu_p;            % eccentricity of hyperbola
         vP = h/R;                       % velocity of hyperobola at perigee 
-        dvR = vP - Vc;                  % required delta-v from parking orbit
+        dvR = abs(vP - Vc);             % required delta-v from parking orbit
         Beta = acos(1/e);               % angle between apse line and asymptote line for hyperbola
         sma = h^2/mu_p/(1-e^2);         % Smei-major axis 
         DELTA = (R+abs(sma))*sin(Beta); % aiming radius
